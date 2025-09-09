9330ae3fac371f8625ae4b2bddb99db0d96c4945
szirtes-spengergasse <szi240822@spengergasse.at>
2025-09-09 21:08:35 +0200

    Initial sharing of project

---
+++ StudentInDerSpengergasse.java
@@ -0,0 +1,37 @@
+public class StudentInDerSpengergasse
+{ 
+    private String  name;
+    private int     alter;
+    private boolean matura;
+    
+    public String getName()
+    {
+        return name;
+    
+    }
+    
+    public int getAlter()
+    {
+        return alter;
+    }
+    
+    public boolean getMatura()
+    {
+        return matura;
+    }
+    
+    public void setName(String setName)
+    {
+        name = setName;
+    }
+    
+    public void setAlter(int setAlter)
+    {
+        alter = setAlter;
+    }
+    
+    public void setMatura(boolean setMatura)
+    {
+        matura = setMatura;
+    }
+}    
